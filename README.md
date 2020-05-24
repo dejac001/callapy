d0b0da8dd4bbee639a5af7b47376b1b32cb52534
Robert DeJaco <dejac001@umn.edu>
2020-05-24 19:19:52 -0400

    Add configuration files

---
+++ README.md
@@ -0,0 +1,7 @@
+callapy
+=======
+
+A package for calculating the amount adsorbed
+into solids from liquid-phase batch measurements.
+
+Coming soon!
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="callapy",
+    version="0.0.1",
+    author="Robert F. DeJaco",
+    author_email="dejac001@umn.edu",
+    description="Calculating the amount adsorbed from liquid-phase adsorption measurements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dejac001/callapy",
+    packages=['callapy'],
+    classifiers=[
+        "Development Status:: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    python_requires='>=3.6',
+    install_requires=["matplotlib==3.2.1", "numpy==1.18.4", "pandas==1.0.3"]
+)
