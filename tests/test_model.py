26c344deece81932523a1277243925de73846bf4
Robert DeJaco <dejac001@umn.edu>
2020-05-24 22:59:30 -0400

    Update test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,9 @@
-from callapy.model import DataPoint
+from callapy.model import Model
 import pytest
 
 
 def test_model():
-    with pytest.raises(TypeError):
-        I = DataPoint()
+    with pytest.raises(Exception):
+        I = Model()
 
 # todo: test that get same results with all floats vs all np arrays
