572d662e8cca0cec45db3bc222fb7d94596d0763
Robert DeJaco <dejac001@umn.edu>
2020-05-24 20:58:09 -0400

    Add version to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ author = 'Robert F. DeJaco'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
+version = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
